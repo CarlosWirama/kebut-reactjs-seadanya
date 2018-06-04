8a225328faea37858f1f0338cd29b13bbaa4cf74
CarlosWirama <vincentius_carlos@yahoo.co.id>
2018-06-05 02:26:46 +0700

    styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      //
+      language: 'ID',
     };
   }
 
@@ -16,12 +16,12 @@ export default class App extends React.Component {
     return (
 
       <BrowserRouter>
-        <div >
+        <div id="page-wrapper">
           <header>
-            <TopBar/>
+            <TopBar language={this.state.language}/>
             <NavBar/>
           </header>
-          <div className="container">
+          <main className="container valign-wrapper">
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/menu1" component={Menu1} />
@@ -34,7 +34,7 @@ export default class App extends React.Component {
               <Route path="/register" component={Register} />
               <Route component={NotFound}/>
             </Switch>
-          </div>
+          </main>
           <Footer/>
         </div>
       </BrowserRouter>
@@ -42,17 +42,17 @@ export default class App extends React.Component {
   }
 }
 
-const NotFound = () => <h1 className="valign-wrapper"> 404 Page Not Found </h1>
+const NotFound = () => <h1> 404 Page Not Found </h1>
 
-const Home = () => <div class="valign-wrapper center-align"><h1> Content HOME </h1></div>
-const Menu1 = () => <div class="valign-wrapper center-align"><h1> Content MENU-1 </h1></div>
-const Menu2 = () => <div class="valign-wrapper center-align"><h1> Content MENU-2 </h1></div>
-const Menu3 = () => <div class="valign-wrapper center-align"><h1> Content MENU-3 </h1></div>
-const Menu4 = () => <div class="valign-wrapper center-align"><h1> Content MENU-4 </h1></div>
-const Menu5 = () => <div class="valign-wrapper center-align"><h1> Content MENU-5 </h1></div>
-const Menu6 = () => <div class="valign-wrapper center-align"><h1> Content MENU-6 </h1></div>
-const Login = () => <div class="valign-wrapper center-align"><h1> Login </h1></div>
-const Register = () => <div class="valign-wrapper center-align"><h1> Register </h1></div>
+const Home = () => <h1> Content HOME </h1>
+const Menu1 = () => <h1> Content MENU-1 </h1>
+const Menu2 = () => <h1> Content MENU-2 </h1>
+const Menu3 = () => <h1> Content MENU-3 </h1>
+const Menu4 = () => <h1> Content MENU-4 </h1>
+const Menu5 = () => <h1> Content MENU-5 </h1>
+const Menu6 = () => <h1> Content MENU-6 </h1>
+const Login = () => <h1> Login </h1>
+const Register = () => <h1> Register </h1>
 
 const NavBar = props => (
   <div style={Styles.navBar}>
@@ -76,21 +76,21 @@ const Footer = props => (
   <footer style={Styles.footerContainer} className="row">
   
     <div className="col m4">
-      <h6>What is Cat Ipsum?</h6>
+      <h6><b>What is Cat Ipsum?</b></h6>
       <p style={Styles.footerParagraph}>
         Cat ipsum dolor sit amet, sit in window and stare oooh, a bird, yum hit you unexpectedly, yet shove bum in owner's face like camera lens. Get video posted to internet for chasing red dot cat mojo so Gate keepers of hell, or claw your carpet in places everyone can see - why hide my amazing artistic clawing skills?. Love you, then bite you.
       </p>
     </div>
   
     <div className="col m4">
-      <h6>Why do we use it?</h6>
+      <h6><b>Why do we use it?</b></h6>
       <p style={Styles.footerParagraph}>
         Steal the warm chair right after you get up leave fur on owners clothes my left donut is missing, as is my right. Spend six hours per day washing, but still have a crusty butthole. Gnaw the corn cob plays league of legends sit and stare, and lie in the sink all day, but stand in doorway, unwilling to chose whether to stay in or go out so meow and russian blue. Bleghbleghvomit my furball really tie the room together eat all the power cords.
       </p>
     </div>
 
     <div className="col m4">
-      <h6>Where can I get some?</h6>
+      <h6><b>Where can I get some?</b></h6>
       <a href="http://www.catipsum.com" target="_blank">
         Link
       </a><br/>
@@ -111,10 +111,13 @@ const TopBar = props => (
   <div style={Styles.topbar}>
     <div className="container">
       <div className="right">
+        <div style={Styles.languageMenu}>
+          Language&nbsp;&nbsp;|&nbsp;&nbsp;{props.language}
+        </div>
         <Link to="/login" className="waves-effect waves-light" {...props}>
           Login
         </Link>
-        <span> / </span>
+        <span>&nbsp;&nbsp;/&nbsp;&nbsp;</span>
         <Link to="/register" className="waves-effect waves-light" {...props}>
           Register
         </Link>
@@ -146,5 +149,10 @@ const Styles = {
     backgroundColor: 'black',
     color: 'white',
     display: 'block',
+  },
+  languageMenu: {
+    marginRight: 40,
+    color: 'white',
+    display: 'inline'
   },
 }
